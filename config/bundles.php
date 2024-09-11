c55b1d0865bfeab076cb631ba169d8e85d8fb40b
AnaisP974 <anaisperigny31@gmail.com>
2024-09-11 16:52:51 +0200

    ajout de style avec tailwindCSS

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
 ];
